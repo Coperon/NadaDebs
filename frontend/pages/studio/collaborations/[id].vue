b984986e8eff18f6db9c847427c5db787f3eb7d1
Jordi Molins <jordimolinsv@gmail.com>
2025-05-16 12:10:25 +0200

    collab layout no js

--- frontend/pages/studio/collaborations/[id].vue
+++ frontend/pages/studio/collaborations/[id].vue
@@ -1,30 +1,30 @@
 <template>
     <div class="pt-[3.25rem] sm:pt-[4.25rem]">
-        <article class="pb-12">
-            <div class="px-4 pt-16 flex flex-col gap-6">
+        <article class="pb-12 xl:pb-0 xl:h-[calc(100vh-4.25rem)] xl:flex xl:overflow-auto">
+            <div class="px-4 sm:px-6 lg:px-8 xl:px-12 pt-16 xl:py-8 flex flex-col gap-6 xl:w-[40vw] xl:shrink-0 xl:sticky xl:left-0">
                 <h1 class="text-h2 uppercase flex flex-col gap-1.5">
                     <span class="font-light">Nada Debs x</span>
                     <span>{{ collaborationData?.title }}</span>
                 </h1>
 
-                <div v-if="collaborationData?.description">
+                <div v-if="collaborationData?.description" class="max-w-[62ch]">
                     {{ collaborationData?.description }}
                 </div>
 
-                <div v-if="collaborationData?.year" class="flex flex-col gap-1.5">
-                    <span class="text-a2-bold uppercase">Year</span>
-                    <span class="text-a2">{{ collaborationData?.year }}</span>
+                <div v-if="collaborationData?.year" class="text-h2 flex flex-col gap-1.5 xl:mt-auto">
+                    <span class="uppercase">Year</span>
+                    <span class="font-light">{{ collaborationData?.year }}</span>
                 </div>
             </div>
 
-            <div v-if="collaborationData?.images && collaborationData?.images?.length > 0" class="mt-12 flex flex-col gap-2.5">
+            <div v-if="collaborationData?.images && collaborationData?.images?.length > 0" class="mt-12 xl:mt-0 flex flex-col xl:flex-row gap-2.5 xl:shrink-0 xl:relative xl:bg-sand">
                 <div v-for="image in collaborationData?.images" :key="image._key">
                     <CommonMediaImage
                         :image="image"
                         :alt="image.alt"
                         width="1536"
                         mobileWidth="768"
-                        class="w-full h-auto"
+                        class="w-full h-auto xl:w-auto xl:h-full xl:shrink-0"
                     />
                 </div>
             </div>
