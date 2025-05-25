ad5a863f20350e4446a5bb4e0f4551890cd5060b
Jordi Molins <jordimolinsv@gmail.com>
2025-05-25 09:01:44 +0200

    page header min-height

--- frontend/components/common/PageHeader.vue
+++ frontend/components/common/PageHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="py-20 sm:py-25 lg:py-30 px-4 flex flex-col items-center text-center gap-6">
+    <header class="py-20 sm:py-25 lg:py-30 lg:min-h-[28.75rem] px-4 flex flex-col items-center justify-center text-center gap-6">
         <h1 class="text-h1-mobile sm:text-h1 uppercase">{{ title }}</h1>
         <div class="max-w-[62ch] mx-auto">
             {{ description }}
