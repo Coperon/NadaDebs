d4913d488d5544910d7e4a455536354f88eb06f6
Jordi Molins <jordimolinsv@gmail.com>
2025-05-30 19:08:10 +0200

    fix

--- frontend/pages/connect/contact-us.vue
+++ frontend/pages/connect/contact-us.vue
@@ -18,6 +18,7 @@
 
             <div class="px-4 sm:px-6 lg:px-8 xl:px-12 max-w-screen-sm mx-auto md:w-1/2">
                 <form name="contact" method="POST" data-netlify="true">
+                    <input type="hidden" name="form-name" value="contact" />
                     <div class="grid grid-cols-1 gap-3">
                         <CommonFormInput type="text" name="first-name" placeholder="First Name *" required />
                         <CommonFormInput type="text" name="last-name" placeholder="Last Name *" required />
@@ -28,7 +29,7 @@
                         <CommonFormTextarea name="message" placeholder="Message *" required />
                     </div>
                     <div class="mt-6 flex items-center gap-2">
-                        <input type="checkbox" id="terms" class="appearance-none border border-black/20 rounded-none bg-transparent w-3.5 h-3.5 shrink-0 relative checked:after:content-[''] checked:after:absolute checked:after:top-1/2 checked:after:left-1/2 checked:after:w-1.5 checked:after:h-1.5 checked:after:bg-black checked:after:rounded-full checked:after:translate-x-[-50%] checked:after:translate-y-[-50%]" required />
+                        <input type="checkbox" name="terms" id="terms" class="appearance-none border border-black/20 rounded-none bg-transparent w-3.5 h-3.5 shrink-0 relative checked:after:content-[''] checked:after:absolute checked:after:top-1/2 checked:after:left-1/2 checked:after:w-1.5 checked:after:h-1.5 checked:after:bg-black checked:after:rounded-full checked:after:translate-x-[-50%] checked:after:translate-y-[-50%]" required />
                         <label for="terms" class="text-a2">I have read and agreed to Nada Debs <NuxtLink :to="`/info/${siteSettingsData?.termsAndConditions?.slug?.current}`" target="_blank" class="underline underline-offset-2">Terms & Conditions</NuxtLink> and <NuxtLink :to="`/info/${siteSettingsData?.privacyPolicy?.slug?.current}`" target="_blank" class="underline underline-offset-2">Privacy Policy</NuxtLink></label>
                     </div>
 
