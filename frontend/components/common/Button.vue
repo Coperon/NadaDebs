def8b1a7b1ab2eff06837f17bbda69dca09d8094
Jordi Molins <jordimolinsv@gmail.com>
2025-06-13 18:17:39 +0200

    css

--- frontend/components/common/Button.vue
+++ frontend/components/common/Button.vue
@@ -1,7 +1,7 @@
 <template>
     <div 
         class="h-11 flex items-center justify-center px-14 text-a2 uppercase border rounded-full transition-colors duration-300"
-        :class="isSecondary ? 'font-light bg-transparent border-grey/50 hover:border-grey' : 'font-medium bg-primary-button border-primary-button hover:bg-white'"
+        :class="isSecondary ? 'font-light bg-transparent border-grey/50' : 'font-medium bg-primary-button border-primary-button'"
     >
         <slot />
     </div>
