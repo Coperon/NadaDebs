d08dbf53b9f5770af60bdb7de5ed1c55641356d0
Jordi Molins <jordimolinsv@gmail.com>
2026-05-20 10:16:13 +0200

    smaller buttons

--- frontend/components/common/Button.vue
+++ frontend/components/common/Button.vue
@@ -1,6 +1,6 @@
 <template>
     <div 
-        class="h-11 flex items-center justify-center px-14 text-a2 uppercase bg-transparent border rounded-full transition-colors duration-300"
+        class="h-9 flex items-center justify-center px-10 text-a2 uppercase bg-transparent border rounded-full transition-colors duration-300"
         :class="
             isDisabled ? 'font-light border-light-grey cursor-not-allowed pointer-events-none' : 'font-medium border-black hover:bg-black hover:text-white'
         "
